86c8a3201a31f6ca97ec23664f32e22f796a80f8
Batuhan Urush <120557202+qualityhunt@users.noreply.github.com>
2023-01-26 21:07:11 +0300

    update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { app, electron, BrowserWindow } = require("electron");
 const my_driver_1 = require("./my_driver");
+const button = document.getElementById("btn");
+button.addEventListener("click", driverTest);
 let mainWindo;
 app.on('ready', function() {
     mainWindo = new BrowserWindow({
@@ -12,6 +14,17 @@ app.on('ready', function() {
     mainWindo.on('closed', function() {
         mainWindo = null;
     });
+    async function driverTest() {
+        let my_driver = new my_driver_1.MyDriver();
+        my_driver.startChrome();
+        await my_driver.driver.get("https://google.com");
+        await my_driver.driver.sleep(1000);
+        await my_driver.driver.quit();
+    }
+    driverTest();
 });
+
+
+
 
 //# sourceMappingURL=index.js.map
